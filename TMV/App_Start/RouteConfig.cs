cb60a70b010bd8d21048bd577b9c4d3990a9d234
Trần Huy <harukikb@gmail.com>
2019-03-28 02:31:56 +0700

    huy push gioi thieu

--- TMV/App_Start/RouteConfig.cs
+++ TMV/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace TMV
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                defaults: new { controller = "Home", action = "About", id = UrlParameter.Optional },
                 namespaces: new string[] { "TMV.Controllers" }
 
             );
--- TMV/Controllers/HomeController.cs
+++ TMV/Controllers/HomeController.cs
@@ -15,16 +15,9 @@ namespace TMV.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
+          
             return View();
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
